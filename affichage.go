241f964ed01bf69fe85407b4d5184c077749dcf6
mehdifandi1 <mehdi.fandi@ynov.com>
2022-11-04 12:14:54 +0100

    azerty

---
+++ affichage.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n int
+	fmt.Println("donner nombre")
+	fmt.Scanln(&n)
+	for i := n; i > 0; i-- {
+		for j := 1; j <= i; j++ {
+			fmt.Println("*")
+		}
+		fmt.Println()
+	}
+}
---
+++ azerty.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var mot1 , mot2 string
+	var d,f int
+
+	fmt.print("ecrire phrase 1:")
+    fmt.Scanln(&mot1)
+    fmt.Println("ecrire phrase2")
+
+	for i := 0;i<len(mot1);i++{
+if mot1[i] !=
+
+	}
+
+
+}
+ 
